5f6993df801739111185f9ea88bdd821ac9a1bf0
森田梓帆里 <moritashihori@moritaazusahosatonoMacBook-Air.local>
2022-01-05 23:19:11 +0900

    if

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-let str1 = 'Hello';
-let str2 = 'World!!';
-alert(str1+str2);
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段はりんごより安い');
+}else if (orange == apple){
+  alert('みかんとリンゴが同じ値段');
+}else{
+  alert('みかんの値段がりんごより高い');
+}
