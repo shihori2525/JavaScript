30e159b84bce036a0f401d953d834b5815409b2c
森田梓帆里 <moritashihori@moritaazusahosatonoMacBook-Air.local>
2022-01-06 21:42:37 +0900

    [function]4

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力してください');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
 
 let js_hand = getJShand();
-
 let judge = winLose(user_hand,js_hand);
 
-alert('あなたの選んだ手は'+user_hand+'です。\njavaScriptの選んだ手は'+js_hand+'です。\n結果は'+judge+'です。');
+
+if (user_hand != null){
+  alert('あなたの選んだ手は'+user_hand+'です。\njavaScriptの選んだ手は'+js_hand+'です。\n結果は'+judge+'です。');
+} else {
+  alert("またチャレンジしてね")
+}
 
 function getJShand(){
   let js_hand_num = Math.floor(Math.random()*3 );
@@ -49,6 +57,7 @@ function winLose(user,js){
     winLoseStr = "あいこ";
   }
 }
+
 
 return winLoseStr;
 }
