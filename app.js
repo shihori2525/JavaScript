58849f83e30c5973bd6a3cdab6e49dc89732d602
森田梓帆里 <moritashihori@moritaazusahosatonoMacBook-Air.local>
2022-01-06 13:52:08 +0900

    Input dialog

--- app.js
+++ app.js
@@ -1,9 +1,2 @@
-let alertString;
-alertString = addString("WebCamp");
-
-alert(alertString);
-
-function addString(strA){
-  let addStr = "Hello"+strA;
-  return addStr;
-}
+let promptStr = prompt('何か好きな文字を入力してください。');
+ alert(promptStr);
